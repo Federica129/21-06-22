d046aee5f421830d1ab35dff640a2be2d9ac1cee
Federica129 <schillacifederica99@gmail.com>
2022-06-21 16:12:40 +0200

    chore: esercizio

---
+++ script.js
@@ -0,0 +1,47 @@
+// --- Esercizi 21-06
+
+// Scriviamo una funzione che passata al map, dato un array di nomi torna una lista di "indice + Nome"
+// es:
+// const names = ['Luca', 'Gabriele'];
+// namesToList(names) // -> ['1 - Luca', '2 - Gabriele']
+
+//Scriviamo una funzione che passata al map, converta ogni elemento al propri indice inverso. es:
+
+//const names = ['Luca', 'Gabriele', 'Richard', 'Roberta'];
+//namesToList(names) // -> [3, 2, 1, 0];
+
+// Scriviamo una funzione che passata al map, dato un array ne torni uno al contrario
+// es:
+// const names = ['Luca', 'Gabriele'];
+// namesToList(names) // -> ['Gabriele', 'Luca'];
+
+//ESERCIZIO 1
+(function () {
+  const names = ["Roberta", "Luca", "Gabriele", "Anna", "Paolo"];
+
+  const indexAndNames = names.map((e, i, array) => i + "-" + e);
+
+  console.log(indexAndNames);
+})();
+
+//ESERCIZIO 2
+(function () {
+  const names = ["Roberta", "Luca", "Gabriele", "Anna", "Paolo"];
+
+  const reverseIndex = names.map(
+    (element, index, array) => array.length - 1 - index
+  );
+
+  console.log(reverseIndex);
+})();
+
+//ESERCIZIO 3
+(function () {
+  const names = ["Roberta", "Luca", "Gabriele", "Anna", "Paolo"];
+
+  const reverselist = names.map(
+    (element, index, array) => names[array.length - 1 - index]
+  );
+
+  console.log(reverselist);
+})();
